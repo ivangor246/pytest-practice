5116cf3c5b07f26104d0a0343e4b97b8c6e0f6b2
ivangor246 <ivangor246@gmail.com>
2025-07-11 17:10:48 +0300

    Setup config

---
+++ src/core/config.py
@@ -0,0 +1,35 @@
+import os
+from dataclasses import dataclass
+from functools import lru_cache
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).parent.parent.parent
+
+load_dotenv(BASE_DIR / '.env')
+
+
+@dataclass
+class Config:
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_PORT: str = os.getenv('DB_PORT')
+    DB_NAME: str = os.getenv('DB_NAME')
+    DB_USER: str = os.getenv('DB_USER')
+    DB_PASS: str = os.getenv('DB_PASS')
+
+    @property
+    def SYNC_DB_URL(self) -> str:
+        return f'postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    @property
+    def ASYNC_DB_URL(self) -> str:
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+
+@lru_cache
+def get_config() -> Config:
+    return Config()
+
+
+config = get_config()
