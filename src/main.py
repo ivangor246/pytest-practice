66578d61e3a7bfc7ca768ce94a36775d87fa8736
ivangor246 <ivangor246@gmail.com>
2025-07-13 15:02:05 +0300

    Setup running sync data loader

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import asyncio
+from datetime import date
 
 from core.database import drop_models, init_models
+from utils.loaders import start_sync_data_loader
 
 
 async def main():
+    start_date = date(year=2025, month=7, day=5)
+
     await drop_models()
     await init_models()
+    start_sync_data_loader(start_date)
 
 
 if __name__ == '__main__':
