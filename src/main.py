e7a4796eb2f42a5803402638a84d48c7c47fb142
ivangor246 <ivangor246@gmail.com>
2025-07-13 15:49:08 +0300

    Setup running async data loader

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 import asyncio
 from datetime import date
+from time import time
 
 from core.database import drop_models, init_models
-from utils.loaders import start_sync_data_loader
+from utils.loaders import start_async_data_loader, start_sync_data_loader
 
 
 async def main():
-    start_date = date(year=2025, month=7, day=5)
+    start_date = date(year=2025, month=4, day=1)
 
     await drop_models()
     await init_models()
+
+    start = time()
     start_sync_data_loader(start_date)
+    delta = time() - start
+    print(f'start_sync_data_loader() was completed for {delta:.4f} seconds')
+
+    await drop_models()
+    await init_models()
+
+    start = time()
+    await start_async_data_loader(start_date)
+    delta = time() - start
+    print(f'start_async_data_loader() was completed for {delta:.4f} seconds')
 
 
 if __name__ == '__main__':
