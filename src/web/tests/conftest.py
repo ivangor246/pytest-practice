a55e9f1a2c405866a63520677fb9868267a29cce
ivangor246 <ivangor246@gmail.com>
2025-07-29 21:41:00 +0300

    Create test for get_dates

--- src/web/tests/conftest.py
+++ src/web/tests/conftest.py
@@ -1,7 +1,7 @@
 from typing import AsyncGenerator
 
 import pytest_asyncio
-from app.core.database import get_session
+from app.core.database import engine, get_session
 from app.models.base import Base
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -16,6 +16,10 @@ async def session() -> AsyncGenerator[AsyncSession, None]:
 
 @pytest_asyncio.fixture(autouse=True)
 async def clean_database(session: AsyncSession):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
     for table in reversed(Base.metadata.sorted_tables):
         await session.execute(text(f'TRUNCATE TABLE "{table.name}" RESTART IDENTITY CASCADE'))
+
     await session.commit()
--- src/web/tests/services/test_trading_service.py
+++ src/web/tests/services/test_trading_service.py
@@ -1,7 +1,51 @@
+from datetime import date
+
 import pytest
+import pytest_asyncio
+from app.models.trading import TradingResult
+from app.services.trading import TradingService
+from sqlalchemy import desc, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@pytest_asyncio.fixture
+async def fill_trading_data(session: AsyncSession) -> None:
+    trading_data = []
+    for i in range(1, 11):
+        trading_data.append(
+            TradingResult(
+                exchange_product_id=f'epid{i}',
+                exchange_product_name=f'epn{i}',
+                oil_id=f'oid{i}',
+                delivery_basis_id=f'dbid{i}',
+                delivery_basis_name=f'dbn{i}',
+                delivery_type_id=f'dtid{i}',
+                volume=i,
+                total=i,
+                count=i,
+                date=date(2025, 1, i % 30),
+            )
+        )
+    session.add_all(trading_data)
+    await session.commit()
 
 
 @pytest.mark.asyncio
 class TestTradingService:
-    async def test_sample(self):
-        assert True
+    async def test_get_dates(self, session: AsyncSession, fill_trading_data):
+        service = TradingService(session)
+
+        service_result = await service.get_dates(limit=10, offset=0)
+        service_result_dates = [item.date for item in service_result]
+
+        stmt = (
+            select(TradingResult.date)
+            .distinct()
+            .order_by(desc(TradingResult.date))
+            .limit(10)
+            .offset(0)
+        )
+        db_result = await session.scalars(stmt)
+        db_result_dates = db_result.all()
+
+        assert service_result_dates == db_result_dates
